ed44652001f292a4da798d0aed318a9c6b4a3fa6
José Fumanal <jfumanal92@gmail.com>
2020-12-02 14:35:57 +0100

    configureViews func

--- GaminHood/GameView/GameViewController.swift
+++ GaminHood/GameView/GameViewController.swift
@@ -27,7 +27,13 @@ class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureViews()
     }
     
+    private func configureViews() {
+        gameImage.configure()
+        gameScore.configure()
+    }
    
 }
