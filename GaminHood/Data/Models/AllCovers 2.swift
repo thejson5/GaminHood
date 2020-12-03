9984d01b2775cecf0b93ffab57cd4b6b5f80fc21
Luis Villanueva <luisvillanueva97@gmail.com>
2020-12-03 11:54:03 +0100

    Rober merge

---
+++ GaminHood/Data/Models/AllCovers 2.swift
@@ -0,0 +1,14 @@
+//
+//  AllCovers.swift
+//  GaminHood
+//
+//  Created by Rober Garrido on 2/12/20.
+//
+
+import Foundation
+
+struct AllCovers: Codable {
+    var id: Int
+    var imagePlatform: String
+    var name: String
+}
